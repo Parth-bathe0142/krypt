e88af812707b57e49b7b65dde5a9ca66d7e331d4
Parth-Bathe0142 <parth.bathe0142@gmail.com>
2026-05-15 18:25:06 +0530

    updated server url in build.rs

--- client/build.rs
+++ client/build.rs
@@ -2,12 +2,12 @@ fn main() {
     let profile = std::env::var("PROFILE").unwrap_or_default();
 
     let server_url = match profile.as_str() {
-        "release" => "https://krypt-server-mdkb4odz.fermyon.app",
+        "release" => "https://krypt.fermyon.app",
         _ => "http://localhost:3000",
     };
 
     println!("cargo:rustc-env=SERVER_URL={}", server_url);
 
-    // Re-run this script if the build profile changes
+    println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rerun-if-env-changed=PROFILE");
 }
